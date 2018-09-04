725b4af2bb07121634b6a54d85719f0f6f102ed1
Jul Jen <juliajen.prog@gmail.com>
2018-09-04 23:33:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ function findMatching(array, name) {
  // takes an array of drivers and a string as arguments, each element of the drivers array is a JavaScript object that has a property of name. The function should return each element whose name property matches the provided string argument.
 
  function matchName(collection, name) {
-   return collection.filter(function (driver) {
-     return collection.map(driver => driver.name);
+   return collection.(driver => driver.name);
    })
  }
